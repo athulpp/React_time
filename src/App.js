e12ea16fb757edb1edf0ff6968a9967a26eef3e7
athulppnext <108659242+athulppnext@users.noreply.github.com>
2022-11-12 11:11:06 +0530

    accordion collapse

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/Navbar';
-import Textform from './components/Textform';
+// import Textform from './components/Textform';
+import About from './components/About';
 
 function App() {
   return (
    <>
    <Navbar title='TextUtils'aboutText='About TextUtils'/>
    <div class="container"  >
-   <Textform headings="Enter the Text Please"></Textform>
+   {/* <Textform headings="Enter the Text Please"></Textform> */}
    </div>
-   
+   <About/>
    </>
   );
 }
